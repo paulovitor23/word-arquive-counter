8512484ce1fcdf9c364f9b3e3fc25f6a609bc3c9
Paulo Vitor <pvdoederlein@gmail.com>
2026-05-01 16:14:23 -0300

    feat: implementacao da biblioteca command para facilitar input

--- src/cli.js
+++ src/cli.js
@@ -2,23 +2,41 @@ import fs from 'fs';
 import trataErros from './trataErros.js';
 import {contaPalavras} from './index.js';
 import { montaSaidaArquivo } from './helpers.js';
+import { Command } from 'commander';
 
+const program = new Command();
+
+program
+.version('0.0.1')
+.option('-t, --texto <string>','caminho do texto a ser processado')
+.option('-d, --destino <string>', 'caminho da pasta que salva o arquivo resultados')
+.action((options) => {
+    const {texto, destino} = options 
+    if (!texto || !destino){
+        console.error("erro! favor inserir caminho de origem e destino!")
+        program.help();
+        return;
+    }
+})
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 const endereco = caminhoArquivo[3];
 
-
-fs.readFile(link,'utf-8', (erro,texto)=>{
+function processaArquivo(texto, destino){
+    fs.readFile(texto,'utf-8', (erro,texto)=>{
     try{
         if(erro) throw erro;
         const resultado = contaPalavras(texto);
-        criaESalvaArquivo(resultado, endereco)
+        criaESalvaArquivo(resultado, destino)
     }
     catch(erro){
         trataErros(erro);
     }
 })
+}
+
+
 
 async function criaESalvaArquivo(listaPalavras, enderecoArquivo){
     const arquivoNovo = `${enderecoArquivo}/resultado.txt`
